6736acd9f8bddc250c8b3d42705c9e742ac50777
Faizan Rafieuddin <frafieuddin@GleasonWorksRochester.onmicrosoft.com>
2025-03-06 09:12:35 -0500

    Add project files.

---
+++ DataGeneratorPlot/UserControls/Chart3D.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Mvvm.CodeGenerators;
+
+namespace DataGeneratorPlot.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Chart3D.xaml
+    /// </summary>
+    public partial class Chart3D : System.Windows.Controls.UserControl
+    {
+        public Chart3D()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataGeneratorPlot/UserControls/Chart3DViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DataGeneratorPlot.Models;
+using DevExpress.Mvvm.CodeGenerators;
+using System.Collections.ObjectModel;
+
+namespace DataGeneratorPlot.UserControls
+{
+    [GenerateViewModel]
+    public partial class Chart3DViewModel
+    {
+        public ObservableCollection<ChartSeries> DataPoints { get; set; }
+
+        public Chart3DViewModel()
+        {
+            DataPoints = new ObservableCollection<ChartSeries>
+            {
+                new ChartSeries(1, 2, 3),
+                new ChartSeries(3, 4, 5),
+                new ChartSeries(5, 6, 7)
+            };
+        }
+    }
+}
+
